4ff885fd7bc98a0f317db1efb009a8c230621d24
Harold-VO <haroldvo1327@gmail.com>
2025-05-14 13:12:30 -0600

    Que son los trait, diferencia y ejemplo

--- index.php
+++ index.php
@@ -1,31 +1,58 @@
 <?php
-    abstract class ClaseAbstracta
-    {
-        abstract protected function getValor();
-        abstract public function valorPrefijo($prefijo);
+    /*La herencia multiple no existe en PHP, para que una clase hija herede de varias clases se usan los trait.
+        Nota: la precedencia de metodos o propiedades afecta la informacion que recupera la clase instanciada.*/
+    class Persona{
+        public $nombre, $apellido;
 
-        //El metodo al no ser abstract, no es necesario declararlo en la clase que extiende la presente clase
-        public function imprimir(){
-            echo $this->getValor();
+        public function setNombre($nombre, $apellido){
+            $this->nombre = $nombre;
+            $this->apellido = $apellido;
+        }
+
+        public function getNombre(){
+            return ucwords($this->nombre . " " . $this->apellido);
+        }
+
+        public function saludo(){
+            echo "Hola desde clase Padre.";
         }
     }
 
-    //Las clases abstractas pueden ser extendidas por otras clases
-    class ClaseConcreta extends ClaseAbstracta
-    {
-        //Los metodos abstractos deben declararse
-        protected function getValor()
-        {
-            return "Clase concreta";
-        }
-
-        public function valorPrefijo($prefijo)
-        {
-            return $prefijo . "Clase concreta";
+    //La palabra reservada trait permite la creacion de estos mecanismos para agrupar funcionalidades en clases que no se pueden heredar
+    trait Latino{
+        public function saludo(){
+            echo "Hola desde clase Latino";
         }
     }
 
-    //IMPORTANTE: las clases abstractas NO se deben instanciar
-    $clase = new ClaseConcreta;
-    $clase->imprimir();
-    echo "<br>" . $clase->valorPrefijo("Prefijo de ");
+    trait Europeo{
+        public function saludo(){
+            echo "Hola desde Europero";
+        }
+
+        //El encapsulamiento puede ser cambiado en la clase que usa el trait
+        protected function pais(){
+            echo "I'm German";
+        }
+    }
+
+    class Humano extends Persona{
+        //Para usar los trait se utiliza la palabra reservada use
+        use Latino, Europeo{
+            //El operador insteadOf permite elegir uno de los metodos con el mismo nombre de diferentes trait
+            Latino::saludo insteadOf Europeo;
+            //El operador as permite cambiar el encapsulamiento de un metodo
+            pais as public;
+        }
+
+        public function setNombre($nombre, $apellido){
+            parent::setNombre($nombre,$apellido);
+        }
+    }
+
+    $persona = new Humano();
+    $persona->setNombre("roberto","gomez");
+    echo $persona->getNombre() . "<br>";
+    $persona->saludo();
+    echo "<br>";
+    $persona->pais();
