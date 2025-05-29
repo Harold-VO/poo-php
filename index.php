5b50ab7ebccc83a8344e6185b51db7bf2c91f588
Harold-VO <haroldvo1327@gmail.com>
2025-05-29 12:58:57 -0600

    Uso de Fluent Interface

--- index.php
+++ index.php
@@ -1,32 +1,27 @@
 <?php
-    class Humano{
-        /* Las propiedades estaticas son utiles para almacenar datos que no necesitan ser unicos para cada instancia como
-            configuraciones, estados de la clase o constantes */
-        public static $nombre = "Pedro";
+    class Persona{
+        public $nombre, $apellidoPaterno, $apellidoMaterno;
 
-        public function saludar(){
-            echo "Hola desde funcion normal.";
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+            return $this;
         }
 
-        /* Los metodos estaticos son utiles para crear o clonar objetos, realizar calculos o acceder a datos compartidos por
-            todas las instancias */
-        public static function saludarPersonalizado(){
-            echo "Hola yo soy " . self::$nombre; //self:: permite acceder a la propiedad estatica de la clase
+        public function setApellidos($apellidoPaterno, $apellidoMaterno){
+            $this->apellidoPaterno = $apellidoPaterno;
+            $this->apellidoMaterno = $apellidoMaterno;
+            return $this;
+        }
+
+        public function imprimirDatos(){
+            echo "Nombre: " . $this->nombre . "<br>";
+            echo "Apellido Paterno: " . $this->apellidoPaterno . "<br>";
+            echo "Apellido Materno: " . $this->apellidoMaterno . "<br>";
         }
     }
-
-    class Persona extends Humano{
-        public function saludoPersona(){
-            echo "Hola desde Persona, yo tambien soy " . parent::$nombre; //parent:: permite acceder a la propiedad estatica de la clase padre
-        }
-    }
-
-    $humano = new Humano;
-    $humano->saludar();
-    echo "<br>";
-
-    Humano::saludarPersonalizado();
-    echo "<br>";
 
     $persona = new Persona;
-    $persona->saludoPersona();
+    /* El patron de diseno Fluent Interface permite encadenar multiples llamadas a metodos en una sola expresion */
+    $persona->setNombre("Hector")
+            ->setApellidos("De"," Leon")
+            ->imprimirDatos();
